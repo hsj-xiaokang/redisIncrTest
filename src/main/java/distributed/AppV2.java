8ecf3fe099f375920a9e334c4111431c0e58772c
hsj-xiaokang <2356899074@qq.com>
2019-11-11 15:58:24 +0800

    当前模块的运用程序加锁，集群发布很多个！！！

--- src/main/java/distributed/AppV2.java
+++ src/main/java/distributed/AppV2.java
@@ -14,6 +14,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 /**
  * redis分布式锁-基于SETNX，「SET if Not eXists」
  * RedisTemplate 对应 setIfAbsent「absent 不存在的意思」 
+ * 当前模块的运用程序加锁，集群发布很多个！！！
  * https://blog.csdn.net/wuzhiwei549/article/details/80692278
  *
  */
@@ -39,22 +40,25 @@ public class AppV2 implements CommandLineRunner{
 		//10个线程都去让他增加，总共1000002次
 		for(long i = 0;i < 1002;i++){
 			FIXED_POOL3.execute(()->{
-				//默认没有获取锁
-				boolean lock = false;
-				try{					
-					lock = stringRedisTemplate.opsForValue().setIfAbsent(LOCKKEY, LOCKKEY_VALUE);					
-					if (lock) {
-						logger.info(String.format("==【%s】===AppV2获取到锁：%s===", Thread.currentThread().getName(),lock));
-						stringRedisTemplate.expire(LOCKKEY,1, TimeUnit.MINUTES); //成功设置过期时间- 在1s之后过期
-					}else {
-//						logger.info("AppV2没有获取到锁，不执行任务!");
-					}
-				}finally{
-					if(lock){	
-						stringRedisTemplate.delete(LOCKKEY);
-//						logger.info("AppV2任务结束，释放锁!");		
-					}else{
-//						logger.info("AppV2没有获取到锁，无需释放锁!");
+				
+				synchronized(this.getClass()){//锁住类，不是实例 Runnable command
+					//默认没有获取锁
+					boolean lock = false;
+					try{					
+						lock = stringRedisTemplate.opsForValue().setIfAbsent(LOCKKEY, LOCKKEY_VALUE);					
+						if (lock) {
+							logger.info(String.format("==【%s】===AppV2获取到锁：%s===", Thread.currentThread().getName(),lock));
+							stringRedisTemplate.expire(LOCKKEY,1, TimeUnit.MINUTES); //成功设置过期时间- 在1s之后过期
+						}else {
+//							logger.info("AppV2没有获取到锁，不执行任务!");
+						}
+					}finally{
+						if(lock){	
+							stringRedisTemplate.delete(LOCKKEY);
+//							logger.info("AppV2任务结束，释放锁!");		
+						}else{
+//							logger.info("AppV2没有获取到锁，无需释放锁!");
+						}
 					}
 				}
 				
